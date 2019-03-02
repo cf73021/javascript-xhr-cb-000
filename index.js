dbccebd6840dc3eafb86b658418c4be8ff3fe7c9
Chris Franklin <cf73021@gmail.com>
2019-03-02 03:19:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ function getRepositories() {
 function showRepositories(event, data){
   console.log(this.reponseText);
 }
+
+function getRepositories(){
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", showRepositories);
+  req.open("GET", "https://api.github.com/users/octocat/repos");
+  req.send();
+}
