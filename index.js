41271a7ea07d63b494f84a3b2ae4f834af30ca1e
Chris Franklin <cf73021@gmail.com>
2019-03-02 03:27:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ function getRepositories() {
 
 function showRepositories(event, data){
   console.log(this.reponseText);
+  let repoList = "<ul>";
+  for (var i = 0; i < this.responseText.length; i++){
+    repoList += "<li>" + this.responseText[i]["name"] + "</li>";
+  }
+  repoList += "</ul>";
+  document.getElementById("repositories").innerHTML = repoList;
 }
 
 function getRepositories(){
